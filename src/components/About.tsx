050e8e6051a6b7414f33932634ec5a563c003622
IamAnthonycruz <Charliecruz1224@gmail.com>
2025-01-10 17:27:21 -0500

    new update

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -37,18 +37,24 @@ const toolboxItems = [
   },
 ];
 const hobbies = [
-  { title: "", emoji: "" },
+  { title: "", emoji: "", left: "5%", top: "5%" },
   {
     title: "",
     emoji: "",
+    left: "50%",
+    top: "5%",
   },
   {
     title: "",
     emoji: "",
+    left: "10%",
+    top: "35%",
   },
   {
     title: "",
     emoji: "",
+    left: "35%",
+    top: "40%",
   },
 ];
 const AboutSection = () => {
@@ -60,46 +66,65 @@ const AboutSection = () => {
           title="A Glimpse Into My World"
           description="Learn more about who I am what I do and what inspires me"
         />
-        <div className="mt-20">
-          <Card className="h-[320px]">
-            <CardHeader
-              title="My reads"
-              description="Explore the books shaping my perspective"
-            />
-            <div className="w-40 mx-auto mt-8">
-              <Image src={bookImage} alt="book cover" />
-            </div>
-          </Card>
-          <Card className="h-[320px] p-0">
-            <CardHeader
-              title="My ToolBox"
-              description="Expore the technologies and tools I use to create digital
+        <div className="mt-20 flex flex-col gap-8">
+          <div className="md:grid md:grid-cols-5 md:gap-8">
+            <Card className="h-[320px] md:col-span-2">
+              <CardHeader
+                title="My reads"
+                description="Explore the books shaping my perspective"
+              />
+              <div className="w-40 mx-auto mt-8">
+                <Image src={bookImage} alt="book cover" />
+              </div>
+            </Card>
+            <Card className="h-[320px] p-0 md:col-span-3">
+              <CardHeader
+                title="My ToolBox"
+                description="Expore the technologies and tools I use to create digital
                 experiences"
-              className="px-6 pt-6"
-            />
-            <ToolboxItems items={toolboxItems} className="mt-6" />
-            <ToolboxItems
-              items={toolboxItems}
-              className="mt-6 -translate-x-1/2"
-            />
-          </Card>
-          <Card>
+                className="px-6 pt-6"
+              />
+              <ToolboxItems items={toolboxItems} className="mt-6" />
+              <ToolboxItems
+                items={toolboxItems}
+                className="mt-6"
+                itemsWrapperClassName="-translate-x-1/2"
+              />
+            </Card>
+          </div>
+          <Card className="h-[320px] p-0 flex flex-col">
             <CardHeader
               title="Beyond the Code"
               description="Explore my interests and hobbies beyond the code"
+              className="px-6 py-6"
             />
-            <div>
+            <div className="relative flex-1">
               {hobbies.map((hobby) => (
-                <div key={hobby.title}>
-                  <span>{hobby.title}</span>
+                <div
+                  key={hobby.title}
+                  className="inline-flex items-center gap-2 px-6 bg-gradient-to-r from-emerald-300 to-sky-400 rounded-full py-1.5 absolute "
+                  style={{
+                    left: hobby.left,
+                    top: hobby.top,
+                  }}
+                >
+                  <span className="font-medium text-gray-950">
+                    {hobby.title}
+                  </span>
                   <span>{hobby.emoji}</span>
                 </div>
               ))}
             </div>
           </Card>
-          <Card>
-            <Image src={mapImage} alt="map" />
-            <Image src={smileMemoji} alt="smile emoji" />
+          <Card className="h-[320px] p-0 relative">
+            <Image
+              src={mapImage}
+              alt="map"
+              className="h-full w-full object-cover object-left-top"
+            />
+            <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 size-20 rounded-full bg-gradient-to-r from-emerald-300 to-sky-400 after:content-[''] after:absolute after:inset-0 after:outline after:outline-2 after:-outline-offset-2 after:rounded-full after:outline-gray-950/30">
+              <Image src={smileMemoji} alt="smile emoji" className="size-20" />
+            </div>
           </Card>
         </div>
       </div>
